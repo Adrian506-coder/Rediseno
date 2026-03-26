ccbdc1ef4ddd75e4bfd430d423f6b18999edcecc
Adrian506-coder <treviadrian18@gmail.com>
2026-03-25 20:53:27 -0500

    Update database connection to use Supabase
    Replaced MySQL connection with PostgreSQL connection to Supabase.

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,12 +1,25 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$password = "";
-$bd = "cantina_chichilo";
+$conn = pg_connect("
+    host=aws-0-us-west-2.pooler.supabase.com
+    port=6543
+    dbname=postgres
+    user=postgres.djzggqztxhphmypsalra
+    password=Adrian506-coder
+");
 
-$conexion = new mysqli($host, $usuario, $password, $bd);
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+if (!$conn) {
+    die("Error de conexión ❌");
+} else {
+    echo "Conectado a Supabase 🚀";
 }
+// $host = "localhost";
+// $usuario = "root";
+// $password = "";
+// $bd = "cantina_chichilo";
+
+// $conexion = new mysqli($host, $usuario, $password, $bd);
+
+// if ($conexion->connect_error) {
+//     die("Error de conexión: " . $conexion->connect_error);
+// }
 ?>
