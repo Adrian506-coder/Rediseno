836a003a84e6832573ecb890fc0be20773dc1644
Adrian506-coder <treviadrian18@gmail.com>
2026-06-08 20:57:33 -0500

    Título en inglés en index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Canti Chichilo</title>
+<title>Can Chichilo</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
